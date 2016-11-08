aa6b043a68b6fe64baeda126b39364b2dca01e89
Erick Lares <Erick Lares>
2016-11-08 14:22:38 -0500

    Initial commit

---
+++ Locatr/app/src/main/java/com/example/eric/locatr/GalleryItem.java
@@ -0,0 +1,42 @@
+package com.example.eric.locatr;
+
+/**
+ * Created by eric on 10/26/2016.
+ */
+
+public class GalleryItem {
+
+    private String mCaption;
+    private String mId;
+    private String mUrl;
+
+    @Override
+    public String toString(){
+        return mCaption;
+    }
+
+    public String getCaption() {
+        return mCaption;
+    }
+
+    public void setCaption(String mCaption) {
+        this.mCaption = mCaption;
+    }
+
+    public String getId() {
+        return mId;
+    }
+
+    public void setId(String mId) {
+        this.mId = mId;
+    }
+
+    public String getUrl() {
+        return mUrl;
+    }
+
+    public void setmUrl(String mUrl) {
+        this.mUrl = mUrl;
+    }
+}
+
